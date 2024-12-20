71f26d0ac9a40707603849f1119329cfd335bbc1
Antonio Romero <tonio93romero@gmail.com>
2024-12-20 17:13:12 -0500

    Deploy to GitHub Pages

--- Desktop/Proyecto Vue/exportarExcelVue/vite.config.js
+++ Desktop/Proyecto Vue/exportarExcelVue/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/ListaProductosVue/',  // Asegúrate de cambiar este valor si tu repositorio tiene otro nombre
+  build: {
+    outDir: 'dist',  // Asegúrate de que la salida esté en dist
+  }
 })
+
+
